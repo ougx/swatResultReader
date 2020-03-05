625bf1134f6ca97308b2488ca5c553caae230e4b
Michael Ou <ougengxin@gmail.com>
2020-03-05 11:34:40 -0600

    Update 60-climate-change.bat

--- 60-climate-change.bat
+++ 60-climate-change.bat
@@ -1,4 +1,4 @@
-C:\Python27\ArcGIS10.6\python.exe climate_change.py ^
+C:\Python27\ArcGIS10.5\python.exe climate_change.py ^
 	"D:\WorkSync\hydrology_swat_lab\LittleCreek1\Scenarios\Default\TxtInOut" ^
 	--temperature 0.0 ^
     	--precipitation 1.0  
