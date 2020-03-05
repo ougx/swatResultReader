c79019fc1d2d6943bd69a2935b2821e3b9715bf0
Michael Ou <ougengxin@gmail.com>
2020-03-05 11:35:01 -0600

    Update 20-plot_flow.bat

--- 20-plot_flow.bat
+++ 20-plot_flow.bat
@@ -1,4 +1,4 @@
-C:\Python27\ArcGIS10.6\python.exe plot_flow.py ^
+C:\Python27\ArcGIS10.5\python.exe plot_flow.py ^
 	"D:\WorkSync\hydrology_swat_lab\LittleCreek1\Scenarios\Default\TxtInOut" ^
 	-b 3 10 ^
     --usgs 07325840  07325850  ^
